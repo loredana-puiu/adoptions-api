3c5a12831ac3e27812ea8bba0993857402d49b3b
Loredana <lore.puiu@yahoo.com>
2021-08-03 22:02:31 +0300

    - new

---
+++ src/main/java/com/p5/adoptions/AdoptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.p5.adoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionApiApplication.class, args);
+	}
+
+}
